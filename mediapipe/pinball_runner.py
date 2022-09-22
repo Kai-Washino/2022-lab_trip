34f17381724fee10ee2b4621835512cd082d9c1d
Kai Washino <79495772+Kai-Washino@users.noreply.github.com>
2022-09-22 13:21:16 +0900

    add game time and fever time

--- mediapipe/pinball_runner.py
+++ mediapipe/pinball_runner.py
@@ -103,17 +103,24 @@ def main():
     tsuchida = pygame.image.load("materials//tsuchida.png")
     gold = pygame.image.load("materials//gold.png")
     fever = pygame.image.load("materials//fever.jpg")
+    feverend = pygame.image.load("materials//feverend.jpg")
     
     x_paddle=250
 
+    font = pygame.font.SysFont(None, 80)
+    font_big = pygame.font.SysFont(None, 200)
 
     score = 0
     BALLSIZE = 30
     BALLSPEED = 10
     BALLQUANTITY = 2
+    TIMELIMIT = 90
     ball_count = 0
     ball_array = []
-    fever_time = False
+    fever_flag = False
+    game_start = False
+    op_flag = False
+    ed_flag = False
 
     for i in range(BALLQUANTITY):
         ball_array.append([[10, 10], [10, 10], [ohnishi, ohnishi_goal]])
@@ -147,9 +154,7 @@ def main():
         ax = fig.add_subplot(111, projection="3d")
         fig.subplots_adjust(left=0.0, right=1, bottom=0, top=1)
 
-    bg= cv.imread('materials/map.jpg') 
-    basket = cv.imread('materials/basket.png')
-
+    starttime = time.time()
     while True:
         display_fps = cvFpsCalc.get()
 
@@ -254,124 +259,147 @@ def main():
         key = cv.waitKey(1)
         if key == 27:  # ESC
             break
-
+        if key == 13:  # Enter
+            game_start = True
         # 画面反映 #############################################################
         cv.imshow('MediaPipe Holistic Demo', debug_image)
-
+        print(fever_flag)
         # ゲーム ###############################################################
         for event in pygame.event.get():
             if event.type==pygame.QUIT: flag=1
+        rest_time = int(TIMELIMIT - time.time() + starttime)
         screen.fill(BLACK)
- 
-        #パドルを描画
-        x_paddle = basuket_position[0]
-        rect = pygame.Rect(x_paddle, 400, 100, 30)
-        pygame.draw.rect(screen, RED, rect)
+        if(not game_start):
+            starttime = time.time()
+            score_text = font_big.render("ENTER", True, (0,255,255))
+            screen.blit(score_text, (200,100))
         
-        # 障害物を描画
+        elif(fever_flag and  (time.time() - fever_start < 3 or time.time() - fever_start > 18)): #フィーバータイム
+            fever_time = time.time() - fever_start
+            if(fever_time < 3):
+                screen.blit(fever, (100, 100))
+                temp = BALLQUANTITY
+                BALLQUANTITY = 50
+                for i in range(BALLQUANTITY- temp):
+                    ball_array.append([[10, 10], [10, 10], [ohnishi, ohnishi_goal]])
+            elif(fever_time > 18):
+                screen.blit(feverend, (100, 100))
+                BALLQUANTITY = 2
+                if(fever_time > 21):
+                    fever_flag = False
+        elif(rest_time <= 0):
+            score_text = font_big.render("score " + str(score), True, (0,255,255))
+            screen.blit(score_text, (200,100))
+        else:
+            
+            #パドルを描画
+            x_paddle = basuket_position[0]
+            rect = pygame.Rect(x_paddle, 400, 100, 30)
+            pygame.draw.rect(screen, RED, rect)
+            
+            # 障害物を描画
 
-        ball_array = make_obstacle(240, 300, ball_array, screen, WHITE,BALLSIZE, BALLQUANTITY)
-        ball_array = make_obstacle(210, 200, ball_array, screen, WHITE,BALLSIZE, BALLQUANTITY)
-        ball_array = make_obstacle(110, 350, ball_array, screen, WHITE,BALLSIZE, BALLQUANTITY)
-        ball_array = make_obstacle(610, 220, ball_array, screen, WHITE,BALLSIZE, BALLQUANTITY)
-        ball_array = make_obstacle(580, 320, ball_array, screen, WHITE,BALLSIZE, BALLQUANTITY)
+            ball_array = make_obstacle(240, 300, ball_array, screen, WHITE,BALLSIZE, BALLQUANTITY)
+            ball_array = make_obstacle(210, 200, ball_array, screen, WHITE,BALLSIZE, BALLQUANTITY)
+            ball_array = make_obstacle(110, 350, ball_array, screen, WHITE,BALLSIZE, BALLQUANTITY)
+            ball_array = make_obstacle(610, 220, ball_array, screen, WHITE,BALLSIZE, BALLQUANTITY)
+            ball_array = make_obstacle(580, 320, ball_array, screen, WHITE,BALLSIZE, BALLQUANTITY)
 
-        #ボールを描画
+            #当たり判定
+            for i in range(BALLQUANTITY):
+                if(ball_array[i][1][1]==390 and ball_array[i][1][0]>=(x_paddle-10) and ball_array[i][1][0]<=(x_paddle+95)):
+                    ball_count += 1
+                    print(ball_count)
+                    ball_array[i][2][1].play(0)
 
-        #当たり判定
-        for i in range(BALLQUANTITY):
-            if(ball_array[i][1][1]==390 and ball_array[i][1][0]>=(x_paddle-10) and ball_array[i][1][0]<=(x_paddle+95)):
-                ball_count += 1
-                print(ball_count)
-                ball_array[i][2][1].play(0)
+                    if(ball_array[i][2][0] == gold and not fever_flag):
+                        fever_flag = True
+                        fever_start = time.time()
+                        starttime = starttime + 6
+                    elif(ball_array[i][2][0] == tsukamoto): score += 10
+                    elif(ball_array[i][2][0] == terada): score += 5
+                    else: score += 1
 
-                if(ball_array[i][2][0] == gold and not fever_time):
-                    print("金")
-                    temp = BALLQUANTITY
-                    BALLQUANTITY = 50
-                    for i in range(BALLQUANTITY- temp):
-                        ball_array.append([[10, 10], [10, 10], [ohnishi, ohnishi_goal]])
-                    screen.blit(fever, (100, 100))
-                    fever_time = True
-                    time.sleep(3)
-                elif(ball_array[i][2][0] == tsukamoto): score += 10
-                elif(ball_array[i][2][0] == terada): score += 5
-                else: score += 1
+                    ball_array[i][1][0] = random.randrange(700)
+                    ball_array[i][1][1] = 10
+                    ball_array[i][0][0] = BALLSPEED * (random.randrange(0, 3, 2) - 1)
+                    ball_array[i][0][1] = BALLSPEED
+                    
 
-                ball_array[i][1][0] = random.randrange(700)
-                ball_array[i][1][1] = 10
-                ball_array[i][0][0] = BALLSPEED * (random.randrange(0, 3, 2) - 1)
-                ball_array[i][0][1] = BALLSPEED
-                
+                    if(ball_count%3 == 0 and not fever_flag):
+                        ball_array[i][2][0] = gold
+                        ball_array[i][2][1] = gold_goal
+                    elif(ball_count%20 ==0):
+                        ball_array[i][2][0] = tsukamoto
+                        ball_array[i][2][1] = tsukamoto_goal
+                    elif(ball_count%15 ==0):
+                        ball_array[i][2][0] = terada
+                        ball_array[i][2][1] = terada_goal
+                    elif(ball_count%2 ==0):
+                        ball_array[i][2][0] = ohnishi
+                        ball_array[i][2][1] = ohnishi_goal
+                    else:
+                        ball_array[i][2][0] = tsuchida
+                        ball_array[i][2][1] = tsuchida_goal
+                    
 
-                if(ball_count%50 == 0 and not fever_time):
-                    ball_array[i][2][0] = gold
-                    ball_array[i][2][1] = gold_goal
-                elif(ball_count%20 ==0):
-                    ball_array[i][2][0] = tsukamoto
-                    ball_array[i][2][1] = tsukamoto_goal
-                elif(ball_count%15 ==0):
-                    ball_array[i][2][0] = terada
-                    ball_array[i][2][1] = terada_goal
-                elif(ball_count%2 ==0):
-                    ball_array[i][2][0] = ohnishi
-                    ball_array[i][2][1] = ohnishi_goal
-                else:
-                    ball_array[i][2][0] = tsuchida
-                    ball_array[i][2][1] = tsuchida_goal
-                
+                #終了判定
+                if(ball_array[i][1][1]>500):
+                    ball_array[i][1][0] = random.randrange(700)
+                    ball_array[i][1][1] = 10
+                    ball_array[i][0][0] = BALLSPEED* (random.randrange(0, 3, 2) - 1)
+                    ball_array[i][0][1] = BALLSPEED
+                    ball_count += 1
 
-            #終了判定
-            if(ball_array[i][1][1]>500):
-                ball_array[i][1][0] = random.randrange(700)
-                ball_array[i][1][1] = 10
-                ball_array[i][0][0] = BALLSPEED* (random.randrange(0, 3, 2) - 1)
-                ball_array[i][0][1] = BALLSPEED
-                ball_count += 1
+                    if(ball_count%3 ==0 and not fever_flag):
+                        ball_array[i][2][0] = gold
+                        ball_array[i][2][1] = gold_goal
+                    elif(ball_count%20 ==0):
+                        ball_array[i][2][0] = tsukamoto
+                        ball_array[i][2][1] = tsukamoto_goal
+                    elif(ball_count%15 ==0):
+                        ball_array[i][2][0] = terada
+                        ball_array[i][2][1] = terada_goal
+                    elif(ball_count%2 ==0):
+                        ball_array[i][2][0] = ohnishi
+                        ball_array[i][2][1] = ohnishi_goal
+                    else:
+                        ball_array[i][2][0] = tsuchida
+                        ball_array[i][2][1] = tsuchida_goal
 
-                if(ball_count%50 ==0):
-                    ball_array[i][2][0] = gold
-                    ball_array[i][2][1] = gold_goal
-                elif(ball_count%20 ==0):
-                    ball_array[i][2][0] = tsukamoto
-                    ball_array[i][2][1] = tsukamoto_goal
-                elif(ball_count%15 ==0):
-                    ball_array[i][2][0] = terada
-                    ball_array[i][2][1] = terada_goal
-                elif(ball_count%2 ==0):
-                    ball_array[i][2][0] = ohnishi
-                    ball_array[i][2][1] = ohnishi_goal
-                else:
-                    ball_array[i][2][0] = tsuchida
-                    ball_array[i][2][1] = tsuchida_goal
+                #壁に反射
+                if(ball_array[i][1][0]>=700): 
+                    ball_array[i][0][0] = -BALLSPEED
+                    bound.play(0)
+                if(ball_array[i][1][0]<=0): 
+                    ball_array[i][0][0] = BALLSPEED
+                    bound.play(0)
+                if(ball_array[i][1][1]<=0):
+                    ball_array[i][0][1] = BALLSPEED
+                    bound.play(0)
+                ball_array[i][1][0] += ball_array[i][0][0]
+                ball_array[i][1][1] += ball_array[i][0][1]
+                # pygame.draw.circle(screen, WHITE, (ball_array[i][1][0], ball_array[i][1][1]), BALLSIZE)
 
-            #壁に反射
-            if(ball_array[i][1][0]>=700): 
-                ball_array[i][0][0] = -BALLSPEED
-                bound.play(0)
-            if(ball_array[i][1][0]<=0): 
-                ball_array[i][0][0] = BALLSPEED
-                bound.play(0)
-            if(ball_array[i][1][1]<=0):
-                ball_array[i][0][1] = BALLSPEED
-                bound.play(0)
-            ball_array[i][1][0] += ball_array[i][0][0]
-            ball_array[i][1][1] += ball_array[i][0][1]
-            # pygame.draw.circle(screen, WHITE, (ball_array[i][1][0], ball_array[i][1][1]), BALLSIZE)
-
-            screen.blit(ball_array[i][2][0], (ball_array[i][1][0] - BALLSIZE, ball_array[i][1][1] - BALLSIZE))
-  
-        #スコアを表示
-        font = pygame.font.SysFont(None, 80)
-        score_text = font.render(str(score), True, (0,255,255))
-        screen.blit(score_text, (630,10))
+                screen.blit(ball_array[i][2][0], (ball_array[i][1][0] - BALLSIZE, ball_array[i][1][1] - BALLSIZE))
     
+            #スコアを表示
+            score_text = font.render("score " + str(score), True, (0,255,255))
+            screen.blit(score_text, (470,50))
+
+            #残り時間を表示
+            time_text = font.render("time " + str(rest_time), True, (0,255,255))
+            screen.blit(time_text, (470,10))
+
         pygame.display.flip()
         myclock.tick(60)
 
+    
+
+
     cap.release()
     cv.destroyAllWindows()
-
+            
 
 def overlayImage(src, overlay, location):
     overlay_height, overlay_width = overlay.shape[:2]
